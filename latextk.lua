a7162e1d73093a974913fbb961f521bc483652c6
grwells <garrettrwells@gmail.com>
2024-08-20 13:30:18 -0700

    changed bib compile arg from -w to -b, broke arguments into steps so they can be combined without redundancy, see ltk -bgv

--- latextk.lua
+++ latextk.lua
@@ -27,63 +27,70 @@
 --   *--> scripts/
 --]]
 
-function make_directory_structure (base) 
- -- make directory structure, substituting in base as project name
-    mkdir_cmd_str = string.format("mkdir -p ./%s/src ./%s/include/pictures ./%s/include/diagrams ./%s/bibliography", base, base, base, base)
-    os.execute(mkdir_cmd_str)
- -- create main latex file
-    os.execute(string.format("touch ./%s/src/main.tex", base))
+function make_directory_structure(base)
+	-- make directory structure, substituting in base as project name
+	mkdir_cmd_str = string.format(
+		"mkdir -p ./%s/src ./%s/include/pictures ./%s/include/diagrams ./%s/bibliography",
+		base,
+		base,
+		base,
+		base
+	)
+	os.execute(mkdir_cmd_str)
+	-- create main latex file
+	os.execute(string.format("touch ./%s/src/main.tex", base))
 end
 
-function clone_latex_toolkit ()
-    -- clone toolkit from github
-    os.execute([[git clone https://github.com/grwells/latex-toolkit.git ./scripts]])
+function clone_latex_toolkit()
+	-- clone toolkit from github
+	os.execute([[git clone https://github.com/grwells/latex-toolkit.git ./scripts]])
 end
 
 function file_exists(name)
-   local f=io.open(name,"r")
-   if f~=nil then io.close(f) return true else return false end
+	local f = io.open(name, "r")
+	if f ~= nil then
+		io.close(f)
+		return true
+	else
+		return false
+	end
 end
 
-
-function create_new_project (base) 
-    -- create a new LaTeX project directory
-    txt = string.format("Project Name: %s ", base)
-    cap = string.rep("*", string.len(txt) + 2)
-    io.write(cap, "\n", txt, " *\n", cap, "\n")
-    make_directory_structure(base)
-    -- clone_latex_toolkit()
+function create_new_project(base)
+	-- create a new LaTeX project directory
+	txt = string.format("Project Name: %s ", base)
+	cap = string.rep("*", string.len(txt) + 2)
+	io.write(cap, "\n", txt, " *\n", cap, "\n")
+	make_directory_structure(base)
+	-- clone_latex_toolkit()
 end
 
+function infer_project_name(t)
+	-- default function params
+	-- all calls to this function must be of form infer_project_name{...}
+	setmetatable(t, { __index = { silent = true } })
 
-function infer_project_name (t)
-    -- default function params
-    -- all calls to this function must be of form infer_project_name{...}
-    setmetatable(t, {__index={silent=true}})
+	local fp = io.popen("basename $PWD", "r")
+	local base = fp:read("*a")
+	base = string.sub(base, 1, -2) -- remove newline character
+	fp:close()
 
-    local fp = io.popen("basename $PWD", "r")
-    local base = fp:read("*a")
-    base = string.sub(base, 1, -2) -- remove newline character
-    fp:close()
+	txt = string.format("Project Name: %s", base)
+	cap = string.rep("*", string.len(txt) + 2)
 
-    txt = string.format("Project Name: %s", base)
-    cap = string.rep("*", string.len(txt) + 2)
-    
-    if not silent then
-        io.write(cap, "\n", txt, " *\n", cap, "\n")
-    end
+	if not silent then
+		io.write(cap, "\n", txt, " *\n", cap, "\n")
+	end
 
-    return base
+	return base
 end
 
-local argparse = require "argparse"
+local argparse = require("argparse")
 
 -- [[
 -- Define arguments and flags for this program to be printed by argparse.
 -- ]]
-local parser = argparse() 
-    :name "LaTeX - Toolkit"
-    :description [[
+local parser = argparse():name("LaTeX - Toolkit"):description([[
 ██╗      █████╗ ████████╗███████╗██╗  ██╗              ████████╗██╗  ██╗
 ██║     ██╔══██╗╚══██╔══╝██╔════╝╚██╗██╔╝              ╚══██╔══╝██║ ██╔╝
 ██║     ███████║   ██║   █████╗   ╚███╔╝     █████╗       ██║   █████╔╝ 
@@ -93,80 +100,61 @@ local parser = argparse()
                                                                         
 
 A script for autogenerating, evaluating, and managing any LaTeX project. 
-]]
 
-parser:flag "-c --word-count"
-    :description "print word count summary by section"
-    :action(
-        function(args)
-            os.execute([[texcount ./src/main.tex]])
-        end
-    )
+Simple Usage -> ltk -b -g -v
+1. compiles bibliography
+2. compiles/generates pdf
+3. opens pdf in system pdf viewer
+]])
 
-parser:flag "-g --generate-pdf"
-    :description "compile and generate a pdf of the latex document in ./src/ using pdflatex command"
-    :action(
-        function(args)
-            os.execute([[cd ./src/ && pdflatex main.tex]])
-        end
-    )
+parser:flag("-c --word-count"):description("print word count summary by section"):action(function(args)
+	os.execute([[texcount ./src/main.tex]])
+end)
 
-parser:flag "-p --print-pdf"
-    :description "compile and generate a pdf and print on default printer"
-    :action(
-        function(args)
-            os.execute([[cd ./src/ && pdflatex main.tex]])
-            os.execute([[lpr ./src/main.pdf]])
-        end
-    )
+parser
+	:flag("-g --generate-pdf")
+	:description("compile and generate a pdf of the latex document in ./src/ using pdflatex main.tex")
+	:action(function(args)
+		os.execute([[cd ./src/ && pdflatex main.tex]])
+	end)
 
-parser:flag "-o --open-document" 
-    :description "open the LaTeX document in ./src/ in NeoVim for editing"
-    :action(
-        function(args)
-            os.execute([[nvim +TZAtaraxis ./src/main.tex]])
-        end
-    )
+parser:flag("-p --print-pdf"):description("print generated pdf on default printer"):action(function(args)
+	os.execute([[lpr ./src/main.pdf]])
+end)
 
-parser:flag "-v --view" 
-    :description "open generated pdf without compiling"
-    :action(
-        function(args)
-            os.execute([[xdg-open ./src/main.tex]])
-        end
-    )
+parser
+	:flag("-o --open-document")
+	:description("open the LaTeX document in ./src/ in NeoVim for editing")
+	:action(function(args)
+		os.execute([[nvim +TZAtaraxis ./src/main.tex]])
+	end)
 
-parser:flag "-w --pdf-view" 
-    :description "generate and open the pdf in the default system pdf viewer"
-    :action(
-        function(args)
-            if file_exists("./src/main.pdf") then
-                io.write("[DEBUG] building for bibtex")
-                os.execute([[cd ./src/ && pdflatex main.tex && bibtex main && pdflatex main.tex && xdg-open main.pdf]])
-            else
-                os.execute([[cd ./src/ && pdflatex main.tex && xdg-open main.pdf]])
-            end
-        end
-    )
+parser
+	:flag("-v --view")
+	:description("open generated pdf in default system pdf viewer without compiling")
+	:action(function(args)
+		os.execute([[xdg-open ./src/main.pdf]])
+	end)
 
-parser:flag "-t --tree-view"
-    :description "list directory structure and files in tree format"
-    :action(
-        function(args)
-            os.execute([[find . | sed -e "s/[^-][^\/]*\//  |/g" -e "s/|\([^ ]\)/|-\1/"]])
-        end
-    )
+parser
+	:flag("-b --build-bibliography")
+	:description("compiles with pdflatex/bibtex to include citation/bibliography")
+	:action(function(args)
+		io.write("[DEBUG] building for bibtex")
+		os.execute([[cd ./src/ && pdflatex main.tex && bibtex main && pdflatex main.tex]])
+	end)
 
-parser:option "-n --new-project" 
-    :description "generate a new project directory and default LaTeX file from scratch"
-    :args "?"     
-    :action(
-        function(args, _, fn)
-            print("project name: ", fn[1])
-            make_directory_structure(fn[1])
-        end
-    )
+parser:flag("-t --tree-view"):description("list directory structure and files in tree format"):action(function(args)
+	os.execute([[find . | sed -e "s/[^-][^\/]*\//  |/g" -e "s/|\([^ ]\)/|-\1/"]])
+end)
 
+parser
+	:option("-n --new-project")
+	:description("generate a new project directory and default LaTeX file from scratch")
+	:args("?")
+	:action(function(args, _, fn)
+		print("project name: ", fn[1])
+		make_directory_structure(fn[1])
+	end)
 
 local args = parser:parse()
-
