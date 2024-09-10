22c39f2c4fbebe41d2ecf6ff8d66407526c7f7a8
grwells <well1157@vandals.uidaho.edu>
2024-09-10 12:32:42 -0700

    added shell escape flag, tree print to new project, and separated biber from pdf gen

--- latextk.lua
+++ latextk.lua
@@ -18,7 +18,9 @@
 --   |           |
 --   |           *--> diagrams/
 --   |           |
---   |           *--> pictures/
+--   |           *--> images/
+--   |                      |
+--   |                      *-->title-background/
 --   |
 --   |
 --   *--> bibliography/
@@ -27,10 +29,14 @@
 --   *--> scripts/
 --]]
 
+-- FLAGS
+-- these are flags for operation, the default unset value is nil, otherwise expect boolean
+enable_shell_escape = nil
+
 function make_directory_structure(base)
 	-- make directory structure, substituting in base as project name
 	mkdir_cmd_str = string.format(
-		"mkdir -p ./%s/src ./%s/include/pictures ./%s/include/diagrams ./%s/bibliography",
+		"mkdir -p ./%s/src ./%s/include/images ./%s/include/diagrams ./%s/bibliography",
 		base,
 		base,
 		base,
@@ -102,20 +108,52 @@ local parser = argparse():name("LaTeX - Toolkit"):description([[
 A script for autogenerating, evaluating, and managing any LaTeX project. 
 
 Simple Usage -> ltk -b -g -v
-1. compiles bibliography
+1. compiles bibliography with biber
 2. compiles/generates pdf
 3. opens pdf in system pdf viewer
 ]])
+
+parser
+	:flag("-b --build-bibliography")
+	:description("compiles with biber to include citation/bibliography, requires pdflatex/latex be run after to generate output")
+	:action(function(args)
+		io.write("[DEBUG] building for bibtex")
+		os.execute([[cd ./src/ && biber main]])
+	end)
 
 parser:flag("-c --word-count"):description("print word count summary by section"):action(function(args)
 	os.execute([[texcount ./src/main.tex]])
 end)
 
 parser
+	:flag("-e --escape-shell")
+	:description("set the -shell-escape flag for pdflatex, this is required for packages such as minted, tikz, etc. to run external tools")
+	:action(function(args)
+		io.write("[DEBUG] LTK setting enabling external tools with -shell-escape option")
+        enable_shell_escape = true
+	end)
+
+parser
 	:flag("-g --generate-pdf")
 	:description("compile and generate a pdf of the latex document in ./src/ using pdflatex main.tex")
 	:action(function(args)
-		os.execute([[cd ./src/ && pdflatex main.tex]])
+
+        if enable_shell_escape == nil then
+            -- enable_shell_escape is undeclared
+            -- compile without shell escape (default)
+            io.write("[DEBUG] shell escape nil")
+            os.execute([[cd ./src/ && pdflatex main.tex]])
+        elseif enable_shell_escape == true then
+            -- enable shell escape
+            io.write("[DEBUG] shell escape true")
+            os.execute([[cd ./src/ && pdflatex --shell-escape main.tex]])
+        elseif enable_shell_escape == false then
+            -- absolutely disable all shell escape dependent behavior
+            -- WARNING: will disable packages such as bibtex
+            io.write("[DEBUG] shell escape false")
+            os.execute([[cd ./src/ && pdflatex --no-shell-escape main.tex]])
+        end
+
 	end)
 
 parser:flag("-p --print-pdf"):description("print generated pdf on default printer"):action(function(args)
@@ -136,25 +174,32 @@ parser
 		os.execute([[xdg-open ./src/main.pdf]])
 	end)
 
-parser
-	:flag("-b --build-bibliography")
-	:description("compiles with pdflatex/bibtex to include citation/bibliography")
-	:action(function(args)
-		io.write("[DEBUG] building for bibtex")
-		os.execute([[cd ./src/ && pdflatex main.tex && biber main && pdflatex main.tex]])
-	end)
 
-parser:flag("-t --tree-view"):description("list directory structure and files in tree format"):action(function(args)
-	os.execute([[find . | sed -e "s/[^-][^\/]*\//  |/g" -e "s/|\([^ ]\)/|-\1/"]])
-end)
+parser
+    :flag("-t --tree-view")
+    :description("list directory structure and files in tree format")
+    :action(function(args)
+        os.execute([[find . | sed -e "s/[^-][^\/]*\//  |/g" -e "s/|\([^ ]\)/|-\1/"]])
+    end)
 
 parser
 	:option("-n --new-project")
 	:description("generate a new project directory and default LaTeX file from scratch")
 	:args("?")
 	:action(function(args, _, fn)
-		print("project name: ", fn[1])
+        project_str = string.format(
+        "\n========================================================\n\tNew LaTeX Project: ./%s\n========================================================",
+        fn[1]
+        )
+		io.write(project_str)
 		make_directory_structure(fn[1])
+        -- print the file tree for the new directory
+        cd_dir_str = string.format(
+            "cd ./%s" ,
+            fn[1]
+        )
+        cd_dir_str = cd_dir_str .. [[ && find . | sed -e "s/[^-][^\/]*\//  |/g" -e "s/|\([^ ]\)/|-\1/"]]
+        os.execute(cd_dir_str)
 	end)
 
 local args = parser:parse()
